8e4b1df7c236fa56ee25db1e6033e11e827d728a
L826 <42918542+L826@users.noreply.github.com>
2020-04-03 15:09:23 -0300

    Update 1006.c

--- Iniciante/1006/1006.c
+++ Iniciante/1006/1006.c
@@ -4,9 +4,9 @@ int main() {
     
     double a, b, c, media;
     
-    scanf("%lf %lf %lf", &a, &b, &c);
+    scanf("%lf%lf%lf", &a, &b, &c);
     media = (2*a + 3*b + 5*c)/10;
-    printf("MEDIA = %.1lf", media);
+    printf("MEDIA = %.1lf/n", media);
 
     return 0;
 }
