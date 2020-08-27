3fddecbdff534bbf2f3227dffdedd71cf41630ba
PauloZanni <zanni97@hotmail.com>
2020-08-27 15:06:11 -0300

    Adicionando 2748

---
+++ Iniciante/2748/2748.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("---------------------------------------\n");
+    printf("|        Roberto                      |\n");
+    printf("|                                     |\n");
+    printf("|        5786                         |\n");
+    printf("|                                     |\n");
+    printf("|        UNIFEI                       |\n");
+    printf("---------------------------------------\n");
+ 
+    return 0;
+}
