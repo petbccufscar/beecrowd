6d9ca04986daa24271e64209a61a2f2cd56718c1
PauloZanni <67290376+PauloZanni@users.noreply.github.com>
2020-09-05 08:47:17 -0300

    Update 2786.c

--- Iniciante/2786/2786.c
+++ Iniciante/2786/2786.c
@@ -4,13 +4,13 @@ int main() {
  
     int Largura,Comprimento,tipo1,tipo2;
 
-    scanf("%d %d",&L,&C);
+    scanf("%d %d",&Largura,&Comprimento);
 
-    type1 = (L+L-1)*(C-1)+L;
-    type2 = ((L-1)*2) + ((C-1)*2);
+    tipo1 = (Largura+Largura-1)*(Comprimento-1)+Largura;
+    tipo2 = ((Largura-1)*2) + ((Comprimento-1)*2);
 
-    printf("%d\n", type1);
-    printf("%d\n", type2);
+    printf("%d\n", tipo1);
+    printf("%d\n", tipo2);
 
     return 0;
 }
