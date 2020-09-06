eb99eaa44f20f466407a115dc6d58843ff554596
Bruna Fernandes <brunafernandesp.bcc@gmail.com>
2020-09-06 01:56:40 -0300

    Atualizando exercício 2763

--- Iniciante/2763/2763.c
+++ Iniciante/2763/2763.c
@@ -5,40 +5,10 @@ int main() {
 	
 	scanf("%d.%d.%d-%d", &X, &Y, &Z, &D);
 	
-	if(X<10)
-		printf("00%d\n",X);
-	else{
-		if(X<100)
-			printf("0%d\n",X);
-		
-		else
-			printf("%d\n",X);
-	}
-	
-	if(Y<10)
-		printf("00%d\n",Y);
-	else{
-		if(Y<100)
-			printf("0%d\n",Y);
-		
-		else
-			printf("%d\n",Y);
-	}
-	
-	if(Z<10)
-		printf("00%d\n",Z);
-	else{
-		if(Z<100)
-			printf("0%d\n",Z);
-		
-		else
-			printf("%d\n",Z);
-	}
-	
-	if(D<10)
-		printf("0%d\n",D);
-	else
-		printf("%d\n",D);
+	printf("%03d\n",X);
+	printf("%03d\n",Y);
+	printf("%03d\n",Z);
+	printf("%02d\n",D);
 	
 	return 0;
 }
