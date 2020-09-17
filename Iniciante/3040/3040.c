4595ccf90b08b53218fba42e08032ba464b4e68c
Amanda <amandamanso7@gmail.com>
2020-09-17 13:02:05 -0300

    Adicionando 3040

---
+++ Iniciante/3040/3040.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int casos, altura, tronco, galhos, i;
+
+    scanf("%d", &casos);
+
+    for (i = 0 ; i < casos; ++i)
+    {
+        scanf("%d %d %d", &altura, &tronco, &galhos);
+
+        if (altura >= 200 && altura <= 300)
+            if (tronco >= 50)
+                if (galhos >= 150)
+                {
+                    printf("Sim");
+                }
+        else
+            printf("Nao");
+
+    }
+
+    return 0;
+
+}
