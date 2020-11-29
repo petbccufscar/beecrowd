b26902c84200ffd97b751dd926b256a82f55b100
Amanda <amanda.lrib@gmail.com>
2020-11-29 18:30:47 -0300

    Update 2253.c
    Removi os comentários.

--- Strings/2253/2253.c
+++ Strings/2253/2253.c
@@ -1,40 +1,40 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
 int main()
 {
     char senha[255];
-    int i, tam;
-    int maiuscula, minuscula, numero, ehInvalida;
+    int i,tam;
+    int maiuscula,minuscula,numero,verifica;
 
     while (gets(senha) != NULL)
     {
-        ehInvalida = 0;
-        numero = 0;
-        maiuscula = 0;
-        minuscula = 0;
+        verifica=0;
+        numero=0;
+        maiuscula=0;
+        minuscula=0;
+        
         tam = strlen(senha);
-
         for (i = 0; i < tam; i++)
         {
             if (senha[i] == 32)
             {
-                ehInvalida = 1;
+                verifica = 1;
                 break;
             }
             else if (strlen(senha) < 6 || strlen(senha) > 32)
             {
-                ehInvalida = 1;
+                verifica = 1;
                 break;
             }
-            else if ((senha[i] < 48) || (senha[i] > 57 && senha[i] < 65) || senha[i] > 122 (senha[i] >= 91 && senha[i] < 97))
+            else if ((senha[i] < 48) || (senha[i] > 57 && senha[i] < 65) || senha[i] > 122)
             {
-                ehInvalida = 1;
+                verifica = 1;
                 break;
             }
             else if (senha[i] >= 91 && senha[i] < 97)
             {
-                ehInvalida = 1;
+                verifica = 1;
                 break;
             }
 
@@ -45,14 +45,14 @@ int main()
             else if (senha[i] > 96 && senha[i] < 123)
                 minuscula = 1;
         }
-
-        if (ehInvalida == 1)
+        
+        if (verifica == 1)
             printf("Senha invalida.\n");
         else if (numero == 0 || (maiuscula == 0 || minuscula == 0))
             printf("Senha invalida.\n");
         else
             printf("Senha valida.\n");
     }
-
+    
     return 0;
 }
