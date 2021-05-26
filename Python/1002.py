7ffc990d1d3f5c27fcdc67927de5f62d686f3898
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2021-05-26 18:11:33 -0300

    URI - 1002

---
+++ Python/1002.py
@@ -0,0 +1,8 @@
+# le o valor do raio pela entrada padrão
+r = float(input())
+
+# calcula o valor conforme a formula fornecida
+area = 3.14159*(r**2)
+
+# imprime o valor na saida padrão com arredondamento de 4 casas decimais
+print('A=%.4f' % area)
