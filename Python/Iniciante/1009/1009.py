a82ef2dc752b5cd7f2c9dcbedfd8acd34a330f28
Vitor Recoaro <50752991+Vitorrecoaro@users.noreply.github.com>
2021-05-27 19:23:39 -0300

    Comentários

--- Python/Iniciante/1009/1009.py
+++ Python/Iniciante/1009/1009.py
@@ -1,7 +1,10 @@
-
-nome = input()
-salario = float(input())
+#Entrada de dados
+nome = input() 
+salario = float(input()) #Uso do caster "float" para forçar a entrada seja float
 total_vendas = float(input())
 
+#Calculo do total recebido pelo vendedor
 total_recebido = salario + (total_vendas*0.15)
+
+#Mostrando ao usuário com duas casas decimais com o "%.2f"
 print("TOTAL = R$ %.2f" %total_recebido)
