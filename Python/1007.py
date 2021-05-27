bb9e6e42575ab902ae6656f7f814bf2291964413
Vanderlei Junior <vanderlei@estudante.ufscar.br>
2021-05-27 15:58:22 -0300

    1007, 1065, 1066

---
+++ Python/1007.py
@@ -0,0 +1,8 @@
+#Recebe os quatro valores e salva cada um em uma variável
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+#Imprime na tela o texto "DIFERENCA =" seguido do resultado pedido no exercício
+print("DIFERENCA =", (A * B - C * D))
---
+++ Python/1065.py
@@ -0,0 +1,14 @@
+# Inicialização da variável 'par' com o valor 0
+par = 0
+
+# 'for' para leitura dos argumentos
+for num in range(1, 5):
+    num = int(input())
+    # Assim que o argumento é recebido, é comparado se
+    # o resto da divisão por 2 é zero, assim, sendo par
+    if((num % 2) == 0):
+        # Se for par, o valor de 'par' é incrementado em 1
+        par += 1
+
+# Imprime na tela a quantidade de pares com o texto "valores pares"
+print(par, "valores pares")
