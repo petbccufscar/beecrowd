c72f62ecd838e03f79b1c561f1695465a3abb5d1
Vitor Recoaro <50752991+Vitorrecoaro@users.noreply.github.com>
2021-05-27 19:28:01 -0300

    Update

--- Python/Iniciante/1019/1019.py
+++ Python/Iniciante/1019/1019.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 segundos = int(input()) #Obtenção de dados apartir da função input, com o caster de inteiro.
+
+#Conversao de segundos para horas e minutos
 horas = segundos/3600 
 segundos = segundos%3600 #Uso do caracter '%' (resto), que calcula o resto da divisão entre segundos e 3600.
 minutos = segundos/60
