2e9d1880bd1763666f6880bec1c0aeb16372a103
Nathan Oliveira <38508159+nathanoliveira19@users.noreply.github.com>
2021-06-05 11:38:53 -0300

    Update 1060.py

--- Python/Iniciante/1060.py
+++ Python/Iniciante/1060.py
@@ -1,3 +1,4 @@
+# Problema 1060 - URI - Iniciante - Nível 1
 #Laço de repetição que lê números e os coloca em uma lista
 #Este for é feito em uma linha e é usado para formar listas
 nums = [(float(input())) for i in range(1,7)]
@@ -8,10 +9,5 @@ nums = [(float(input())) for i in range(1,7)]
 positivos = len([ i for i in nums if i > 0])
 
 #Saída de dados
-print(positivos, 'valores positivos')
-
-#Caso tenha alguma dúvida sobre este problema ou sobre a resolução, entre em contato com o PET-BCC pelo nosso
-#[Facebook](https://www.facebook.com/petbcc/),
-#[Instagram](https://www.instagram.com/petbcc.ufscar/)
-#ou nos mande um e-mail com o assunto "URI" para  petbcc.ufscar@gmail.com
+print(f'{positivos} valores positivos')
 
