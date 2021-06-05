70334603fd70ed8db6d034ba98d77f45f5d2f170
Nathan Oliveira <38508159+nathanoliveira19@users.noreply.github.com>
2021-06-05 11:36:23 -0300

    Update 1003.py

--- Python/Iniciante/1003.py
+++ Python/Iniciante/1003.py
@@ -1,3 +1,4 @@
+# Problema 1003 - URI - Iniciante - Nível 1
 #Leitura dos valores
 A = int(input())
 B = int(input())
@@ -6,9 +7,4 @@ B = int(input())
 SOMA = A + B
 
 #Saída de dados
-print('SOMA =' , SOMA)
-
-#Caso tenha alguma dúvida sobre este problema ou sobre a resolução, entre em contato com o PET-BCC pelo nosso
-#[Facebook](https://www.facebook.com/petbcc/),
-#[Instagram](https://www.instagram.com/petbcc.ufscar/)
-#ou nos mande um e-mail com o assunto "URI" para  petbcc.ufscar@gmail.com
+print(f'SOMA = {SOMA}')
