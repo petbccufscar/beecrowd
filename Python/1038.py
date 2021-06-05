b692f5433f777eb9b89ba3bbb4f24711c3fbad37
Vitor Recoaro <50752991+Vitorrecoaro@users.noreply.github.com>
2021-06-05 10:27:06 -0300

    Update 1038.py

--- Python/1038.py
+++ Python/1038.py
@@ -3,10 +3,6 @@
 #Entrada de dados com a função map para dividir os valores de entrada pelo 'espaço'
 item, qtd_item = map(float, input().split(' ')) 
 
-#Distribuindo os valores de entrada em variáveis para melhor exemplificação.
-item = int(entrada[0])
-qtd_item = int(entrada[1])
-
 #Estrutura condicional para escolher qual será o preço total cobrado.
 if(item==1):
     preco_total = 4.00*qtd_item
