2132a18ebcb781621187272272f1763523be044c
Vitor Recoaro <50752991+Vitorrecoaro@users.noreply.github.com>
2021-06-05 10:20:59 -0300

    Update 1009.py

--- Python/1009.py
+++ Python/1009.py
@@ -1,6 +1,9 @@
+# Problema 1009 - URI - Iniciante - Nível 2
+
 #Entrada de dados
+#Uso do caster "float" na linha 4 e 5 para forçar a entrada seja float
 nome = input() 
-salario = float(input()) #Uso do caster "float" para forçar a entrada seja float
+salario = float(input()) 
 total_vendas = float(input())
 
 #Calculo do total recebido pelo vendedor
