98d2fa6e2c01f22edfb011ceb823766a063d6f2a
Carlos Eduardo Fontaneli <carlosfontaneli42@gmail.com>
2021-06-05 11:43:48 -0300

    Update Python/1015.py

--- Python/1015.py
+++ Python/1015.py
@@ -1,3 +1,5 @@
+# Problema 1015 - URI - Iniciante - Nível 1
+
 # Adicionando "math" para usar  "sqrt" e "pow"
 import math 
 # Lendo x1 e y1
@@ -5,4 +7,4 @@ x1, y1 = input().split(" ")
 # Lendo x2 e y2
 x2, y2 = input().split(" ")
 # Imprimindo a distancia
-print("%.4f" % (math.sqrt(math.pow((float(x2) - float(x1)), 2) + math.pow((float(y2) - float(y1)), 2))))
+print(f'{(math.sqrt(math.pow((float(x2) - float(x1)), 2) + math.pow((float(y2) - float(y1)), 2))):.4f}')
