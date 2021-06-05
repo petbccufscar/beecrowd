6f31bd8f896eec80142fc49270ae0cc26d00ec4c
Carlos Eduardo Fontaneli <carlosfontaneli42@gmail.com>
2021-06-05 12:11:07 -0300

    Update Python/1015.py

--- Python/1015.py
+++ Python/1015.py
@@ -3,8 +3,8 @@
 # Adicionando "math" para usar  "sqrt" e "pow"
 import math 
 # Lendo x1 e y1
-x1, y1 = input().split(" ")
+x1, y1 = map(float, input().split(" "))
 # Lendo x2 e y2
-x2, y2 = input().split(" ")
+x2, y2 = map(float, input().split(" "))
 # Imprimindo a distancia
-print(f'{(math.sqrt(math.pow((float(x2) - float(x1)), 2) + math.pow((float(y2) - float(y1)), 2))):.4f}')
+print(f'{(math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))):.4f}')
