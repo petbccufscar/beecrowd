b061d34602fa03a2f89222f858f14e7b5854f4e8
Vanderlei Junior <vanderlei@estudante.ufscar.br>
2021-06-11 15:56:56 -0300

    Adicionando 1143, 1144 e 1149

---
+++ Python/Iniciante/1143.py
@@ -0,0 +1,9 @@
+# Problema 1143 - URI - Iniciante - Nível 1
+
+# Lê o valor fornecido pela entrada padrão
+valor = int(input())
+
+# X assumirá todos os valores de 0 até o VALOR - 1
+# em seguida, imprime o valor de X, seguido por X ao quadrado e X ao cubo
+for x in range(valor):
+    print(f'{x+1} {(x+1)**2} {(x+1)**3}')
