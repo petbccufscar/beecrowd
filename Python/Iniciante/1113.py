3c27e19997643fe45f709d4fe8eddef95428e97d
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2021-06-10 21:39:31 -0300

    Adicionados: 1113, 1114 e 1115

---
+++ Python/Iniciante/1113.py
@@ -0,0 +1,13 @@
+# Problema 1113 - URI - Iniciante - Nível 1
+
+# Inicializa x e y com valores diferentes
+x, y = 0, 1
+
+# Loop lendo as entradas até serem iguais
+while x != y:
+    # Leitura de multiplas variáveis em uma linha
+    x, y = map(int, input().split(' '))
+    # Caso x e y forem iguais, não haverá print
+    if x != y:
+        # Imprime Crescente se crescente e Decrescente caso contrário
+        print("Crescente" if x < y else "Decrescente")
