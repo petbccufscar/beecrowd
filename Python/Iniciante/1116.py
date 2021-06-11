483ac4213ca195092912a4c166e5f992730f8f21
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2021-06-10 23:37:28 -0300

    Adicionando 1116, 1117 e 1131 [py]

---
+++ Python/Iniciante/1116.py
@@ -0,0 +1,13 @@
+#Recebendo número de divisões na entrada de dados
+N = int(input())
+
+#For que repete N vezes
+for i in range(N):
+    #Aplicação da função float através do map na lista de dois valores (X, Y)
+    X, Y = map(float, input().split(' '))
+    #Caso Y não seja igual a 0, é possível efetuar a divisão
+    if Y != 0:
+        print(X/Y)
+    #Caso contrário, mostrar na tela a mensagem "divisao impossivel"
+    else:
+        print('divisao impossivel')
---
+++ Python/Iniciante/1131.py
@@ -0,0 +1,44 @@
+#Inicialização de contadores de placar
+vitorias_gremio = 0
+vitorias_inter = 0
+empates = 0
+
+#Entrada de placar de gols usando map
+inter, gremio = map(int, input().split(' '))
+
+#Verificação de qual time ganhou para ajustar contadores
+if gremio > inter:
+    vitorias_gremio += 1
+elif gremio < inter:
+    vitorias_inter += 1
+else:
+    empates += 1
+
+#While verificando se o usuário digitou 1 ou 2
+while (continua:= int(input('Novo grenal (1-sim 2-nao)\n'))) == 1:
+    
+    #Entrada de placar de gols usando map
+    inter, gremio = map(int, input().split(' '))
+    
+    #Verificação de qual time ganhou para ajustar contadores
+    if gremio > inter:
+        vitorias_gremio += 1
+    elif gremio < inter:
+        vitorias_inter += 1
+    else:
+        empates += 1
+
+#Exibição dos placares na saída de dados
+print(f'{empates + vitorias_inter + vitorias_gremio} grenais')
+print(f'Inter:{vitorias_inter}')
+print(f'Gremio:{vitorias_gremio}')
+print(f'Empates:{empates}')
+
+#Verificação de qual time ganhou mais e exibição de resultado
+#final na saída de dados
+if vitorias_inter > vitorias_gremio:
+    print('Inter venceu mais')
+elif vitorias_inter < vitorias_gremio:
+    print('Gremio venceu mais')
+else:
+    print('Nao houve vencedor')
