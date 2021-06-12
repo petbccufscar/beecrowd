c7445f9fde441f9469e9a0e4ca861aec2e9e474e
Geovanne Mansano <geovanne_mansano@hotmail.com>
2021-06-12 12:08:33 -0300

    Adicionando 1096, 1097 e 1099

---
+++ Python/1097.py
@@ -0,0 +1,16 @@
+# Problema 1097 - URI - Iniciante - Nível 1
+
+# Declaração das variáveis
+i=1
+j=7
+
+# Início do primeiro loop
+for contador1 in range(5):
+	# Início do segundo loop
+	for contador2 in range(3):
+		# Imprimindo a saída e incrementando a variável 'j'
+ 		print(f'I={i} J={j}')
+ 		j=j-1 
+	# Incremento as variáveis 'i' e 'j'
+	i=i+2
+	j=j+5
