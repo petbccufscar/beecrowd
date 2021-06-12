66ebef3978f4bd6da9dae0c94599dedce3a00c20
donderileo <donderileo@gmail.com>
2021-06-12 13:44:04 -0300

    Adicionando 1150, 1153, 1150

---
+++ Python/Iniciante/1150.py
@@ -0,0 +1,27 @@
+# Problema 1150 - URI - Iniciante - Nível 1
+
+# Começamos recebendo os dois inputs necessários (já convertendo para int), X e Z
+X = int(input())
+Z = int(input())
+
+# Como há a condição de Z ser maior que X, enquanto Z for menor ou igual a X,
+# refazemos a leitura
+while(Z <= X):
+    Z = int(input())
+
+# Com a condição satisfeita, começamos a soma, que começa com o valor de X como descrito no problema.
+soma = X
+
+# Também iniciaremos um contador de iterações (i) e também será uma variável para somar os sucessores de X.
+# 'i' começa em 1, por X ser sempre menor que Z.
+
+i = 1
+#Equanto a soma for menor que X.
+while(soma <= Z):
+
+    soma += X+i
+    #Entrou no loop, incrementamos o contador 
+    i += 1
+
+# Por fim, imprimimos a variável i (número de iterações de soma)
+print(i)
---
+++ Python/Iniciante/1154.py
@@ -0,0 +1,24 @@
+# Problema 1154 - URI - Iniciante - Nível 1
+
+# Começamos lendo a primeira idade (já convertendo para float, já que a média pode ser ponto flutuante)
+idade = float(input())
+
+# A soma das idades começa em 0 pois podemos não ter nenhuma entrada.
+soma = 0
+
+# Também utilizaremos um contador, para saber quantas idades estamos lidando para a média.
+i = 0
+
+# Enquanto a idade for maior que 0, continuaremos lendo idades para o cálculo da média.
+while(idade >= 0):
+
+    # Durante o loop, apenas somamos as idades e recebemos a próxima.
+    soma += idade
+    idade = float(input())
+    i = i + 1
+
+# Por fim, calculamos e imprimimos a média (soma das idades/quantidade de idades)
+media = float(soma/i)
+
+# Limitamos 
+print(f'{media:.2f}')
