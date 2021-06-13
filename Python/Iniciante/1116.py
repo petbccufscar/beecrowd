9dcf383406953e84e1803115e9de85c5a95dc760
Nathan Oliveira <38508159+nathanoliveira19@users.noreply.github.com>
2021-06-13 12:59:16 -0300

    Apply suggestions from code review
    Co-authored-by: Vitor Recoaro <50752991+Vitorrecoaro@users.noreply.github.com>

--- Python/Iniciante/1116.py
+++ Python/Iniciante/1116.py
@@ -1,13 +1,14 @@
-#Recebendo número de divisões na entrada de dados
+# Problema 1116 - URI - Iniciante - Nível 1
+# Recebendo número de divisões na entrada de dados
 N = int(input())
 
-#For que repete N vezes
+# For que repete N vezes
 for i in range(N):
-    #Aplicação da função float através do map na lista de dois valores (X, Y)
+    # Aplicação da função float através do map na lista de dois valores (X, Y)
     X, Y = map(float, input().split(' '))
-    #Caso Y não seja igual a 0, é possível efetuar a divisão
+    # Caso Y não seja igual a 0, é possível efetuar a divisão
     if Y != 0:
         print(X/Y)
-    #Caso contrário, mostrar na tela a mensagem "divisao impossivel"
+    # Caso contrário, mostrar na tela a mensagem "divisao impossivel"
     else:
         print('divisao impossivel')
--- Python/Iniciante/1131.py
+++ Python/Iniciante/1131.py
@@ -1,12 +1,13 @@
-#Inicialização de contadores de placar
+# Problema 1131 - URI - Iniciante - Nível 1
+# Inicialização de contadores de placar
 vitorias_gremio = 0
 vitorias_inter = 0
 empates = 0
 
-#Entrada de placar de gols usando map
+# Entrada de placar de gols usando map
 inter, gremio = map(int, input().split(' '))
 
-#Verificação de qual time ganhou para ajustar contadores
+# Verificação de qual time ganhou para ajustar contadores
 if gremio > inter:
     vitorias_gremio += 1
 elif gremio < inter:
@@ -14,13 +15,13 @@ elif gremio < inter:
 else:
     empates += 1
 
-#While verificando se o usuário digitou 1 ou 2
+# While verificando se o usuário digitou 1 ou 2
 while (continua:= int(input('Novo grenal (1-sim 2-nao)\n'))) == 1:
     
-    #Entrada de placar de gols usando map
+    # Entrada de placar de gols usando map
     inter, gremio = map(int, input().split(' '))
     
-    #Verificação de qual time ganhou para ajustar contadores
+    # Verificação de qual time ganhou para ajustar contadores
     if gremio > inter:
         vitorias_gremio += 1
     elif gremio < inter:
@@ -28,14 +29,14 @@ while (continua:= int(input('Novo grenal (1-sim 2-nao)\n'))) == 1:
     else:
         empates += 1
 
-#Exibição dos placares na saída de dados
+# Exibição dos placares na saída de dados
 print(f'{empates + vitorias_inter + vitorias_gremio} grenais')
 print(f'Inter:{vitorias_inter}')
 print(f'Gremio:{vitorias_gremio}')
 print(f'Empates:{empates}')
 
-#Verificação de qual time ganhou mais e exibição de resultado
-#final na saída de dados
+# Verificação de qual time ganhou mais e exibição de resultado
+# Final na saída de dados
 if vitorias_inter > vitorias_gremio:
     print('Inter venceu mais')
 elif vitorias_inter < vitorias_gremio:
