fae1c278c7e40a490dfe1856a4c631db49d9831e
Fonta42 <fonta42@pop-os.localdomain>
2021-06-17 17:53:07 -0300

    Subindo 1190, 1478, 1534, 2235, 2344, 2483

---
+++ Python/1190.py
@@ -0,0 +1,29 @@
+# Problema 1190 - URI - Iniciante - Nível 1
+
+# Leitura da operação
+operacao = input()
+
+# Criação da matriz e leitura dos números
+matriz = []
+for i in range(12):
+  # Criando as demais dimensões
+  auxiliar = []
+  for j in range(12):
+    auxiliar.append(float(input()))
+  matriz.append(auxiliar)
+
+# Declarando a variável que armazenará o resultado
+resultado = 0
+
+# Realizando a soma dos elementos
+for i in range(12):
+  for j in range(12):
+    if(i<j and i > 12 - j - 1):
+      resultado += matriz[i][j]
+
+# Conferindo qual operação a ser realizada
+if operacao == 'M':
+  resultado = resultado / 30.0
+
+# Imprimindo resultado final
+print(resultado)
---
+++ Python/2483.py
@@ -0,0 +1,14 @@
+# Problema 2383 - URI - Iniciante - Nível 1
+
+# Lendo o índice de felicidade
+felicidade = int(input())
+
+# Imprimindo o começo da mensagem, sem a quebra de linha
+print('Feliz nat', end='')
+
+# Imprimindo os 'a' de felicidade
+for i in range(felicidade):
+    print('a', end='')
+
+# Imprimindo o final da mensagem
+print('l!')
