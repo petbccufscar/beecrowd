d03cf2901992fdf2b8199644815f2627b4ad8482
Carlos Eduardo Fontaneli <carlosfontaneli42@gmail.com>
2021-06-23 08:46:15 -0300

    Update Python/2235.py
    Co-authored-by: Matheus Victorello <40409803+matheusvictorello@users.noreply.github.com>

--- Python/2235.py
+++ Python/2235.py
@@ -1,7 +1,7 @@
 # Problema 2235 - URI - Iniciante - Nível 1
 
 # Leitura dos dados
-a, b, c= map(int, input().split(' '))
+a, b, c = map(int, input().split(' '))
 
 # Comparações para verificação de resultado
 if(a == b or a == c or b == c):
