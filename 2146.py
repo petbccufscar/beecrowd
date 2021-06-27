11ef3086c611af24c8771e7e53efc7aff5282dd4
Geovanne Mansano <geovanne_mansano@hotmail.com>
2021-06-27 19:19:16 -0300

    Add files via upload

---
+++ 2146.py
@@ -0,0 +1,14 @@
+# Problema 2146 - URI - Iniciante - Nível 1
+
+# Criando o loop para ler as variáveis até finalizar o arquivo(EOF)
+
+while True:
+	try:
+		# Leitura da variável 'n'
+		n=int(input())
+
+		# Printando 'n-1'
+		print(n-1)
+
+	except EOFError:
+		break
