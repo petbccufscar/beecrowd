d57ef155ead9a3c33697fd76ad76533b32667ecb
yurissunbali <82149175+yurissunbali@users.noreply.github.com>
2021-07-14 11:00:13 -0300

    Update Python/Iniciante/2950.py
    Co-authored-by: Nathan Oliveira <38508159+nathanoliveira19@users.noreply.github.com>

--- Python/Iniciante/2950.py
+++ Python/Iniciante/2950.py
@@ -1,7 +1,7 @@
 # Inicializa as variáveis e atribui
 # os valores da distancia entre os Palantír's
 # e os seus respectivos diâmetros
-Distancia, Diametro1, Diametro2 = [int(i) for i in input().split()]
+Distancia, Diametro1, Diametro2 = map(int, input().split(' '))
 
 # Imprime a ICM
 print("{:.2f}".format(Distancia/(Diametro1 + Diametro2)))
