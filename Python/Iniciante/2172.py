00796325b5b7fc989fa5010c742c1188687049d6
yurissunbali <82149175+yurissunbali@users.noreply.github.com>
2021-07-14 15:07:35 -0300

    Adicionando 2172, 2176, 2234, 2896, 2936, 2950

---
+++ Python/Iniciante/2172.py
@@ -0,0 +1,13 @@
+# Problema 2172 - URI - Iniciante - Nível 1
+
+# Inicializa as variáveis e atribui
+# os valores de X e EXP nessas variáveis
+X, EXP = map(int, input().split(' '))
+
+# Executa a operação de imprimir o valor de
+# EXP multiplicado por X e atribui novos valores
+# a essas variáveis, continuando o laço
+# enquanto ambas não tiverem valor igual a 0
+while X != 0 and EXP != 0:
+    print(X*EXP)
+    X, EXP = map(int, input().split(' '))
---
+++ Python/Iniciante/2176.py
@@ -0,0 +1,21 @@
+# Problema 2176 - URI - Iniciante - Nível 1
+
+# Inicializa a string contendo mensagem S
+S = input()
+
+# Variável para contar a quantidade de dígitos 1
+digitos = 0
+
+# Verifica se a quantidade de dígitos
+# com valor igual a 1 é par ou não
+for i in S:
+    if i == "1":
+        digitos += 1
+
+# Verifica se a quantidade de dígitos é par,
+# testando se é divisível por 2, ou não,
+# e em seguida imprimindo a mensagem
+if digitos % 2 == 0:
+    print(S + "0")
+else:
+    print(S + "1")
