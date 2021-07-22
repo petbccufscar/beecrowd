9a748e62031345025cd42a8a5fce7fed550a8346
Vanderlei Junior <vanderlei@estudante.ufscar.br>
2021-07-22 15:37:37 -0300

    Adicionando 3047, 3055, 3091

--- Python/Iniciante/2750.py
+++ Python/Iniciante/2750.py
@@ -1,12 +1,5 @@
 # Problema 2750 - URI - Iniciante - Nível 1
 
-# No Python, a impressão de valores OCTAL e HEXADECIMAL tem formatações específicas
-# que impedem a impressão de forma correta, logo, foram criadas 3 listas, cada uma com os valores
-# em suas respecitivas bases
-numerosD = list(range(16))
-numerosO = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17]
-numerosH = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
-
 # Impressão do cabeçalho da tabela
 print("---------------------------------------")
 print("|  decimal  |  octal  |  Hexadecimal  |")
@@ -14,8 +7,16 @@ print("---------------------------------------")
 
 # FOR responsável pela impressão da tabela formatada
 # ':^N' alinha a variável de forma centralizada ocupando N casas.
-for x in range(16):
-    print(f'|  {numerosD[x]:^9}|{numerosO[x]:^9}|{numerosH[x]:^15}|')
+for i in range(0, 16):
+	# String com a representação em hexadecimal
+	# A função 'hex' retorna uma string que começa com '0x', quem não será usada
+	h = hex(i).replace('0x', '').upper()
+	
+	# String com a representação em octal
+	# A função 'oct' retorna uma string que começa com '0o', quem não será usada
+	o = oct(i).replace('0o', '')
+
+	print(f'|  {i:^9}|{o:^9}|{h:^15}|')
 
 # Impressão do rodapé da tabela
 print("---------------------------------------")
---
+++ Python/Iniciante/3047.py
@@ -0,0 +1,12 @@
+# Problema 3047 - URI - Iniciante - Nível 1
+
+# Inicialmente, recebemos os valores da Dona Mônica e dois filhos
+monica = int(input())
+filhoA = int(input())
+filhoB = int(input())
+
+# Calculamos a idade do terceiro filho
+filhoC = monica - (filhoA + filhoB)
+
+# A função max() imprime o maior valor dos argumentos passados
+print(max(filhoA, filhoB, filhoC))
