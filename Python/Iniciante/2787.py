5d4f93c8f431565947a8ab7412c2eeb3e2d85ef6
Geovanne Mansano <geovanne_mansano@hotmail.com>
2021-08-16 14:16:12 -0300

    Add files via upload

---
+++ Python/Iniciante/2787.py
@@ -0,0 +1,17 @@
+# Problema 2787 - URI - Iniciante - Nível 1
+
+# Leitura das variáveis de entrada
+L = int(input())
+C = int(input())
+
+# Verificando os casos baseado nas entradas serem par ou ímpar, e printando o resultado
+if(L%2 == 0):
+	if(C%2 == 0):
+		print(1)
+	else:
+		print(0)
+else:
+	if(C%2 == 0):
+		print(0)
+	else:
+		print(1)
