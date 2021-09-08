a824a6cce378ceefb9d7f4980313e40ed59b05bc
Matheus Victorello <matheusvictorello@gmail.com>
2021-09-08 11:48:39 -0300

    Adicionando 1248, 1253, 1255, 1257

---
+++ Python/String/1248.py
@@ -0,0 +1,24 @@
+# Problema 1248 - URI - String - Nível 2
+
+# Número de casos de teste
+n = int(input())
+
+# Para cada caso
+for _ in range(n):
+	# Criamos um conjunto a partir de cada entrada
+	dieta  = set(input())
+	café   = set(input())
+	almoço = set(input())
+
+	# Fazemos as operações que nos convém com os conjuntos
+	comeu  = café | almoço
+	extra  = comeu - dieta
+	faltou = dieta - comeu
+
+	# Verificamos se algo extra foi comido
+	if extra != set():
+		# Se sim, mostramos 'CHEATER'
+		print('CHEATER')
+	else:
+		# Se não, mostramos o que faltou
+		print(''.join(sorted(faltou)))
---
+++ Python/String/1255.py
@@ -0,0 +1,33 @@
+# Problema 1255 - URI - String - Nível 3
+from collections import defaultdict
+
+# Número de casos de teste
+n = int(input())
+
+# Para cada caso de teste
+for _ in range(n):
+	# Lemos a linha
+	linha = input()
+	# Convertemos todos os caracteres para minúsculas
+	linha = linha.lower()
+	# Filtramos só as letras
+	linha = filter(lambda c : c.isalpha(), linha)
+
+	# Flag para letra mais recorente	
+	maior = 0
+
+	# Contamos as letras
+	contador = defaultdict(int)
+	for c in linha:
+		# Incrementamos o valor atual
+		atual = contador[c] + 1
+		# Verificamos se ele é o novo maior
+		maior = max(maior, atual)
+		# Atribuímos o novo valor atual ao contador
+		contador[c] = atual
+
+	# Filtramos só as letras mais recorentes
+	maiores = filter(lambda c : contador[c] == maior, contador)
+
+	# Printamos em ordem alfabética
+	print(''.join(sorted(maiores)))
