676e1b6f40883dcfc542f92e23b1c0f7c37eae81
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2021-09-27 14:01:30 -0300

    Adicionando 1168, 1222, 1234

---
+++ Python/String/1234.py
@@ -0,0 +1,39 @@
+# Problema 1234 - URI - Strings - Nível 3
+
+while True:
+    try:
+        # Leitura do texto
+        sentence = input()
+        result = ""
+
+        # Controlador da alternância
+        toggle = True
+
+        # Para cada caractere no texto
+        for i in sentence:
+            # Se for uma letra
+            if i.isalpha():
+                # E tiver que ser maiúscula
+                if toggle and i.islower():
+                    # Adiciona a versão maiúscula dela
+                    result += i.upper()
+                # E tiver que ser minúscula
+                elif not toggle and i.isupper():
+                    # Adiciona a versão minúscula dela
+                    result += i.lower()
+                # E tiver certo
+                else:
+                    # Só adicionar
+                    result += i
+
+                toggle = not toggle
+            # Se não for letra
+            else:
+                # Só adicionar
+                result += i
+
+        # Impressão do resultado
+        print(result)
+
+    except EOFError:
+        break
