aebc32ddd56e0beb9702335abed8b413d6fcb6a7
voidptr-foo <82149175+voidptr-foo@users.noreply.github.com>
2021-11-22 02:08:57 -0300

    Update Python/Iniciante/1050.py
    Co-authored-by: Matheus Victorello <40409803+matheusvictorello@users.noreply.github.com>

--- Python/Iniciante/1050.py
+++ Python/Iniciante/1050.py
@@ -8,11 +8,15 @@ existe = False
 
 # Declaração do dicionário com os ddd's e seus respectivos estados
 cidades = {
-        61: "Brasilia", 71: "Salvador",
-        11: "Sao Paulo", 21: "Rio de Janeiro",
-        32: "Juiz de Fora", 19: "Campinas",
-        27: "Vitoria", 31: "Belo Horizonte"
-        }
+        61: "Brasilia",
+        71: "Salvador",
+        11: "Sao Paulo",
+        21: "Rio de Janeiro",
+        32: "Juiz de Fora",
+        19: "Campinas",
+        27: "Vitoria",
+        31: "Belo Horizonte"
+}
 
 # Itera sobre o dicionário em busca do ddd informado
 for i in cidades:
