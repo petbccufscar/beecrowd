da33a25c05d0f193186616981e4c7205c2c8a385
voidptr-foo <82149175+voidptr-foo@users.noreply.github.com>
2021-11-22 04:17:09 -0300

    Update Python/Iniciante/1263.py
    Co-authored-by: Matheus Victorello <40409803+matheusvictorello@users.noreply.github.com>

--- Python/Iniciante/1263.py
+++ Python/Iniciante/1263.py
@@ -9,34 +9,38 @@ while True:
         # Converte todos os caracteres para letras minúsculas
         texto = texto.lower();
 
+        # Separamos cada palavra da linha
+        palavras = texto.split(' ')
+        
+        # Agora pegamos só as primeiras letras
+        letras = map(lambda p : p[0], palavras)
+        
         # Quantidade de aliterações na string
         aliteracoes = 0
 
         # Caractere da palavra predecessora
         caractere = '\0'
 
-        # Contará quantas palavras, consecutivamente,
-        # tem a mesma letra inicial
+        # Contará quantas letras consecutivamente iguais
         contador = 0
 
-        for i in texto.split():
-            if i[0] == caractere:
+        for letra in letras:
+            if letra == caractere:
                 # Incrementa a quantidade de aliterações
-                # somente se for a primeira palavra consecutiva
+                # somente se for a primeira letra consecutiva
                 # a anterior e desconsiderada se, nessa sequência,
-                # aparecerem mais palavras com a mesma letra inicial
+                # aparecerem mais letras iguais
                 if contador == 1:
                     aliteracoes += 1
 
-                # Dado que o caractere é igual a
-                # letra inicial da palavra anterior
+                # Dado que o caractere é igual a letra
                 # incrementa o contador de palavras em sequência
                 contador += 1
             else:
                 # Dado que o caractere é diferente do que estava
                 # armazenado na variável, a sequência é resetada
                 # com o novo caractere
-                caractere = i[0]
+                caractere = letra
 
                 # Reseta o contador e considera a primeira
                 # palavra na sequência de palavras com mesma inicial
