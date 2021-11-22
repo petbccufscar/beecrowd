a8c2db4a170698f30296b47c0403f55492e85dc7
voidptr-foo <82149175+voidptr-foo@users.noreply.github.com>
2021-11-22 02:07:48 -0300

    Update Python/Iniciante/1044.py
    Co-authored-by: Matheus Victorello <40409803+matheusvictorello@users.noreply.github.com>

--- Python/Iniciante/1044.py
+++ Python/Iniciante/1044.py
@@ -1,7 +1,7 @@
 # Problema 1044 - Beeccrowd - Iniciante - Nível 2
 
 # Recebe os valores na entrada padrão
-valorA, valorB = [float(i) for i in input().split()]
+valorA, valorB = map(float, input().split())
 
 # Verifica a divisibilidade para saber se são múltiplos ou não
 if valorA%valorB == 0 or valorB%valorA == 0:
