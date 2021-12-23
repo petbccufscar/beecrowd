6056b7202200594eefd81ee9e737fb460c5fc82a
Geovanne Mansano <geovanne_mansano@hotmail.com>
2021-12-23 09:36:48 -0300

    Update Python/Strings/1276.py
    Co-authored-by: Matheus Victorello <40409803+matheusvictorello@users.noreply.github.com>

--- Python/Strings/1276.py
+++ Python/Strings/1276.py
@@ -1,56 +1,50 @@
-alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# Problema 1276 - Beecrowd - Strings - Nível 3
 
 while True:
     try:
-				# Leitura da entrada com remoção dos espaços vazios
+        # Leitura da entrada com remoção dos espaços vazios
         texto = input().replace(' ', '')
-        letras = []
-
-				# Para cada posição da array texto é inserido seu valor na array 'letra', exceto se este valor já tiver sido inserido
-        for letra in texto:
-            try:
-								# Procura se já esta no vetor letras
-                letras.index(letra)
-            except ValueError:
-								# Caso não esteja é inserido
-                letras.insert(len(letras), letra)
         
+        # Vamos converter o texto em um conjunto, para eliminar as letras repetidas
+        letras = set(texto)
+
+        # Vamos converter o conjunto para uma lista ordenada
+        letras = sorted(list(letras))
+
+        # Se a entrada for vazia apenas printamos uma linha vazia
+        if len(letras) == 0:
+            print()
+            continue
+
+        # Para poder verificar ordem, vamos converter as letras em números
+        numeros = list(map(ord, letras))
+
+        # Definimos o inicío e o fim da sequência como o primeiro número
+        inicio = numeros[0]
+        fim = numeros[0]
+
+        # Lista vazia de faixas
         faixas = []
-				
-				# Organização do vetor em ordem alfabética
-        letras = sorted(letras)
-        primeiraLetra = ''
-        ultimaLetra = ''
-        
-        for letra in letras:
-						# No primeiro loop atualiza o valor das variáveis 'primeiraLetra' e 'ultimaLetra'
-            if primeiraLetra == '':
-                primeiraLetra = letra
-                ultimaLetra = letra
+
+        # Para cada número vemos se ele é a continuação do fim
+        for n in numeros[1:]:
+            # Se sim, incrementamos o fim
+            if n == fim + 1:
+                fim += 1
+            # Se não, terminamos a faiza corrente e iniciamos uma nova com o novo valor
             else:
-								# letra sempe está um valor a frete de 'ultimaletra', então caso sua posição no alfabeto for igual à posição 'ultimaletra'+1, significa que está em ordem alfabética, e portanto 'ultimaletra' é atualizado
-                if alfabeto.index(letra) == alfabeto.index(ultimaLetra) + 1:
-                    ultimaLetra = letra
-                else:
-										# Caso não esteja em ordem alfabética, a faixa de letras é inserido no vetor 'faixas' 
-                    faixas.insert(len(faixas), primeiraLetra + ':' + ultimaLetra)
-                    primeiraLetra = letra
-                    ultimaLetra = letra
+                faixas.append((inicio, fim))
+                inicio = fim = n
 
-				# Inserindo os últimos valores no vetor						
-        if primeiraLetra and ultimaLetra:
-            faixas.insert(len(faixas), primeiraLetra + ':' + ultimaLetra)
-        elif primeiraLetra and ultimaLetra == '':
-            faixas.insert(len(faixas), primeiraLetra + ':' + ultimaLetra)
+        # Precisamos levar em conta a última faixa
+        faixas.append((inicio, fim))
 
-				
-        textoFinal = ''
+        # Como o padrão de saída requer que as faizas sejam separadas por virgula
+        # Vamos converter cada faixa para a notação usada: a:b
+        faixas = map(lambda f : f'{chr(f[0])}:{chr(f[1])}', faixas)
+        
+        # Então printar as faixar unidas por virgula
+        print(', '.join(faixas))
 
-				# Armazena os valores das faixas em 'textoFinal'
-        for faixa in faixas:
-            textoFinal += faixa + ', '
-					
-        # Imprime o texto final sem ', ' final
-        print(textoFinal[0: len(textoFinal) - 2])
     except EOFError:
         break
